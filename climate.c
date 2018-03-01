142807330e95b84279d3bcdd673d67efc415990a
M. Alturki <malturki@usfca.edu>
2018-02-28 21:33:43 -0800

    Add files via upload

--- climate.c
+++ climate.c
@@ -65,40 +65,63 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
+#include <unistd.h>
 #define NUM_STATES 50
 
 /* TODO: Add elements to the climate_info struct as necessary. */
-struct climate_info {
+struct climate_info
+{
     char code[3];
     unsigned long num_records;
     long double sum_temperature;
+    long double humidity;
+    unsigned long snow;
+    unsigned int max_temp;
+    unsigned long max_timestamp;
+    unsigned int min_temp;
+    unsigned long min_timestamp;
+    unsigned long thunder;
+    long double cloud;
 };
 
 void analyze_file(FILE *file, struct climate_info *states[], int num_states);
 void print_report(struct climate_info *states[], int num_states);
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+
+    printf("number arguments = %d\n", argc);
 
     /* TODO: fix this conditional. You should be able to read multiple files. */
-    if (argc != 2) {
+    if (argc <=1)
+    {
         printf("Usage: %s tdv_file1 tdv_file2 ... tdv_fileN \n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    /* Let's create an array to store our state data in. As we know, there are
-     * 50 US states. */
+
+    for(int i=1; i<argc; i++){
+        printf("Opening file: %s\n", argv[i]);
+    }
+
+    /* Let's create an array to store our state data in  */
     struct climate_info *states[NUM_STATES] = { NULL };
-
     int i;
-    for (i = 1; i < argc; ++i) {
+    for (i = 1; i < argc; ++i)
+    {
         /* TODO: Open the file for reading */
-
-        /* TODO: If the file doesn't exist, print an error message and move on
-         * to the next file. */
-
+        char *fname = argv[i];
+        if( access( fname, F_OK ) != -1 )
+        {
+            FILE* file = fopen(fname, "r");
+            analyze_file(file, states, NUM_STATES);
+        }
+        else
+        {
+            printf("File %s does not exist\n", argv[i]);
+            continue;
+        }
         /* TODO: Analyze the file */
-        /* analyze_file(file, states, NUM_STATES); */
     }
 
     /* Now that we have recorded data for each file, we'll summarize them: */
@@ -108,11 +131,15 @@ int main(int argc, char *argv[]) {
 }
 
 /* TODO function documentation */
-void analyze_file(FILE *file, struct climate_info **states, int num_states) {
+void analyze_file(FILE *file, struct climate_info **states, int num_states)
+{
     const int line_sz = 100;
     char line[line_sz];
-    while (fgets(line, line_sz, file) != NULL) {
-
+    const char* delim = "\t";
+    struct climate_info *info;
+    int flag = 0, i = 0, state_index;
+    while (fgets(line, line_sz, file) != NULL)
+    {
         /* TODO: We need to do a few things here:
          *
          *       * Tokenize the line.
@@ -125,15 +152,90 @@ void analyze_file(FILE *file, struct climate_info **states, int num_states) {
          *       * Update the climate_info structure as necessary.
          */
 
+        char *token = strtok(line, delim);
+        char *tokens[9];
+        int idx = 0;
+        while( token != NULL )
+        {
+            tokens[idx++] = token;
+            token = strtok(NULL, delim);
+        }
+        double temp = atof(tokens[8]) * 1.8 - 459.67;
+        int snow = atoi(tokens[4]);
+        unsigned long timestamp = atoi(tokens[1]);
+        int thunder = atoi(tokens[6]);
+        float humidity = atof(tokens[3]);
+        float cloud_cover = atof(tokens[5]);
+
+        char *code = tokens[0];
+        flag = 0;
+
+        for (int i = 0; i < num_states; ++i)
+        {
+            if (states[i] != NULL && strcmp(code, states[i]->code)==0)
+            {
+                info = states[i];
+                state_index = i;
+                flag = 1;
+                break;
+            }
+        }
+        //if flag is zero that means we have not found a state for this record
+        if(flag == 0) 
+        {
+            info = (struct climate_info *)malloc (sizeof(struct climate_info));
+            strcpy(info->code, code);
+            info->cloud = cloud_cover;
+            info->humidity = humidity;
+            info->max_temp=temp;
+            info->max_timestamp = timestamp;
+            info->min_temp = temp;
+            info->min_timestamp = timestamp;
+            info->num_records = 1;
+            info->snow = snow;
+            info->sum_temperature = temp;
+            info->thunder = thunder;
+            for (int i = 0; i < num_states; ++i)
+            {
+                if (states[i] == NULL)
+                {
+                    states[i] = info;
+                    break;
+                }
+            }
+        }
+        //state already exist for this record 
+        else
+        {
+            states[state_index]->num_records += 1;
+            states[state_index]->sum_temperature += temp;
+            states[state_index]->snow += snow;
+            states[state_index]->cloud += cloud_cover;
+            states[state_index]->thunder += thunder;
+            states[state_index]->humidity += humidity;
+
+            if(states[state_index]->min_temp > temp)
+            {
+                states[state_index]->min_temp = temp;
+                states[state_index]->min_timestamp = timestamp;
+            }
+            if(states[state_index]->max_temp < temp)
+            {
+                states[state_index]->max_temp = temp;
+                states[state_index]->max_timestamp = timestamp;
+            }
+        }
     }
 }
 
 /* TODO function documentation */
-void print_report(struct climate_info *states[], int num_states) {
+void print_report(struct climate_info *states[], int num_states)
+{
     printf("States found: ");
-    int i;
-    for (i = 0; i < num_states; ++i) {
-        if (states[i] != NULL) {
+    for (int i = 0; i < num_states; ++i)
+    {
+        if (states[i] != NULL)
+        {
             struct climate_info *info = states[i];
             printf("%s ", info->code);
         }
@@ -141,4 +243,32 @@ void print_report(struct climate_info *states[], int num_states) {
     printf("\n");
 
     /* TODO: Print out the summary for each state. See format above. */
+    for(int i=0; i<num_states; i++)
+    {
+
+        // timestamp = timestamp / 1000;
+        if (states[i] != NULL)
+        {
+            struct climate_info *curr  = states[i];
+
+            //printing values to console
+            float avg_humidity = curr->humidity*1.0/curr->num_records;
+            float avg_temp = curr->sum_temperature*1.0 /curr->num_records;
+
+            printf("-- State: %s --\n", curr->code);
+            
+            printf("Number of Records: %d\n", curr->num_records);
+            printf("Average Humidity: %0.1f\n", (avg_humidity) );            
+            printf("Average Temperature: %0.1fF\n", (avg_temp) );
+
+            printf("Max Temperature: %0.1fF on %s", avg_humidity,  ctime(&(curr->max_timestamp)));
+            printf("Min Temperature: %0.1fF on %s", avg_temp,  ctime(&(curr->min_timestamp)) );
+            printf("Lightning Strikes: %d\n", curr->thunder);
+
+            printf("Records with Snow Cover: %d\n", curr->snow);
+            printf("Average Cloud Cover: %0.1f%\n\n", curr->cloud*1.0/curr->num_records);
+        }
+    }
+
+
 }
